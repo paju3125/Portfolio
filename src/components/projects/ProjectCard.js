23129f2d5650e793c556a365b95262ec75e01740
paju3125 <prajvalgandhi483@gmail.com>
2024-01-08 11:33:45 +0530

    Project section UI Finally done :)

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -4,9 +4,12 @@ export default function ProjectCard(props) {
 
   console.log(props)
   return (
-    <div className="card mx-2">
+    <div className="card mx-2" data-aos="zoom-in-up" data-aos-duration="1000">
       {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path></svg> */}
       <img src={props.img} alt="" />
+      <div className="card-img-overlay d-flex align-items-center justify-content-center">
+        <h5 className="card-title">{props.title}</h5>
+      </div>
       <div className="card__content">
         <p className="card__title">{props.title}</p>
         <p className="card__description" dangerouslySetInnerHTML={{ __html: props.desc }}></p>
