92d67f90d0efb731074e4043eeb095425719c686
JarvisScripter <prajval.gandhi@ignitesol.com>
2024-03-08 14:47:34 +0530

    images changes

--- src/components/Carousel/Journey.js
+++ src/components/Carousel/Journey.js
@@ -9,89 +9,87 @@ export default function Journey() {
                 </h1>
             </div>
             <div className='journey container d-flex align-items-center justify-content-center p-2'>
-                <div style={{ margin: '20px', maxHeight: '700px', width: '80%' }}>
-                    <Carousel className='carousel-fade'>
-                        <Carousel.Item interval={2000}>
+                <Carousel className='carousel-fade'>
+                    <Carousel.Item interval={2000}>
+                        <img
+                            className="d-block w-100"
+                            src="/slider/carrom_comp.jpg"
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h5>Carrom Competition</h5>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+
+                    <Carousel.Item interval={2000}>
+                        <div style={{ overflow: 'hidden' }}>
                             <img
-                                className="d-block w-100"
-                                src="/slider/carrom_comp.jpg"
+                                className="d-block"
+                                src="/slider/runnerup3.jpg"
                                 alt="First slide"
                             />
-                            <Carousel.Caption>
-                                <h5>Carrom Competition</h5>
-                            </Carousel.Caption>
-                        </Carousel.Item>
+                        </div>
+                        <Carousel.Caption>
+                            <h5>Runner's Up - Carrom Competition</h5>
+                        </Carousel.Caption>
+                    </Carousel.Item>
 
-                        <Carousel.Item interval={2000}>
-                            <div style={{ overflow: 'hidden' }}>
-                                <img
-                                    className="d-block"
-                                    src="/slider/runnerup3.jpg"
-                                    alt="First slide"
-                                />
-                            </div>
-                            <Carousel.Caption>
-                                <h5>Runner's Up - Carrom Competition</h5>
-                            </Carousel.Caption>
-                        </Carousel.Item>
+                    <Carousel.Item interval={2000}>
+                        <div style={{ overflow: 'hidden' }}>
+                            <img
+                                className="d-block "
+                                src="/slider/stall.jpg"
+                                alt="First slide"
+                            />
+                        </div>
+                        <Carousel.Caption>
+                            <h5>Profitable Stall</h5>
+                            <p>Our stall got first prize - Victory</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={2000}>
+                        <div style={{ overflow: 'hidden' }}>
+                            <img
+                                className="d-block w-100"
+                                src="/slider/internship_cert.jpg"
+                                alt="First slide"
+                            />
+                        </div>
+                        <Carousel.Caption>
+                            <h5>Project Handover</h5>
+                            {/* <p>The project was successfully completed and delivered to the company.</p> */}
+                        </Carousel.Caption>
+                    </Carousel.Item>
 
-                        <Carousel.Item interval={2000}>
-                            <div style={{ overflow: 'hidden' }}>
-                                <img
-                                    className="d-block "
-                                    src="/slider/stall.jpg"
-                                    alt="First slide"
-                                />
-                            </div>
-                            <Carousel.Caption>
-                                <h5>Profitable Stall</h5>
-                                <p>Our stall got first prize - Victory</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item interval={2000}>
-                            <div style={{ overflow: 'hidden' }}>
-                                <img
-                                    className="d-block w-100"
-                                    src="/slider/internship_cert.jpg"
-                                    alt="First slide"
-                                />
-                            </div>
-                            <Carousel.Caption>
-                                <h5>Project Handover</h5>
-                                {/* <p>The project was successfully completed and delivered to the company.</p> */}
-                            </Carousel.Caption>
-                        </Carousel.Item>
+                    <Carousel.Item interval={2000}>
+                        <div style={{ overflow: 'hidden' }}>
+                            <img
+                                className="d-block w-100"
+                                src="/slider/product_audit.jpg"
+                                alt="First slide"
+                            />
+                        </div>
+                        <Carousel.Caption>
+                            <h5>Product Audit - Mock Interview</h5>
+                            <p></p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
 
-                        <Carousel.Item interval={2000}>
-                            <div style={{ overflow: 'hidden' }}>
-                                <img
-                                    className="d-block w-100"
-                                    src="/slider/product_audit.jpg"
-                                    alt="First slide"
-                                />
-                            </div>
-                            <Carousel.Caption>
-                                <h5>Product Audit - Mock Interview</h5>
-                                <p></p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
+                    <Carousel.Item interval={2000}>
+                        <div style={{ overflow: 'hidden' }}>
+                            <img
+                                className="d-block w-100"
+                                src="/slider/blackbook3.jpg"
+                                alt="First slide"
+                            />
+                        </div>
+                        <Carousel.Caption>
+                            <h5>BlackBook Submission</h5>
 
-                        <Carousel.Item interval={2000}>
-                            <div style={{ overflow: 'hidden' }}>
-                                <img
-                                    className="d-block w-100"
-                                    src="/slider/blackbook3.jpg"
-                                    alt="First slide"
-                                />
-                            </div>
-                            <Carousel.Caption>
-                                <h5>BlackBook Submission</h5>
+                        </Carousel.Caption>
+                    </Carousel.Item>
 
-                            </Carousel.Caption>
-                        </Carousel.Item>
-
-                    </Carousel>
-                </div>
+                </Carousel>
             </div>
         </section >
     )
